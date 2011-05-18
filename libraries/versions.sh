b82b51372d71a2932547d6915feb0e0f43eefe44
Ruben Van Boxem <vanboxem.ruben@gmail.com>
2011-05-18 17:01:45 +0200

    prepare for library builds

---
+++ libraries/versions.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -e
+
+# versions of different packages
+#utility/general
+export NCURSES_VERSION=""
+
+#image/fonts
+export FREETYPE_VERSION=""
+
+#audio/video
+export OGG_VERSION=""
+export VORBIS_VERSION=""
+
+

875c31ecfd19ee5441c44598446b899544db6ca5
Martell Malone <martellmalone@gmail.com>
2012-06-23 01:13:09 +0100

    Try ninja compiler :)

--- toolchain/scripts/llvm-clang.sh
+++ toolchain/scripts/llvm-clang.sh
@@ -6,11 +6,12 @@ then
     echo "--> Already configured"
 else
     echo "--> Configuring"
-    echo `/usr/bin/env python` --version
-    sh $SRC_DIR/LLVM/configure --host=$HOST --build=$BUILD --target=$TARGET --with-sysroot=$PREFIX --prefix=$PREFIX-clang \
-                               --enable-optimized --disable-assertions --disable-pthreads \
-                               CFLAGS="$HOST_CFLAGS" LDFLAGS="$HOST_LDFLAGS" \
-                               > $LOG_DIR/llvm-clang_configure.log 2>&1 || exit 1
+    #echo `/usr/bin/env python` --version
+	sh cmake -G ninja -DCMAKE_C_COMPILER=$HOST-gcc -DCMAKE_CXX_COMPILER=$HOST-g++ -DCMAKE_BUILD_TYPE=Release   
+    #sh $SRC_DIR/LLVM/configure --host=$HOST --build=$BUILD --target=$TARGET --with-sysroot=$PREFIX --prefix=$PREFIX-clang \
+    #                           --enable-optimized --disable-assertions --disable-pthreads \
+    #                           CFLAGS="$HOST_CFLAGS" LDFLAGS="$HOST_LDFLAGS" \
+    #                           > $LOG_DIR/llvm-clang_configure.log 2>&1 || exit 1
     echo "--> Configured"
 fi
 touch configure.marker
